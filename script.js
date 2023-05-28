67b721de71606dc4675f0b653f432a7741b64b91
aks <animeshkumarsrivastava13@gmail.com>
2023-05-28 13:20:28 +0530

    Final project with timer

--- script.js
+++ script.js
@@ -181,9 +181,33 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    //  In each call, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+    // When 0 seconds, stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = `Log in to get started`;
+      containerApp.style.opacity = 0;
+    }
+    // decrease 1s
+    time--;
+  };
+  // Set time to 5 minutes
+  let time = 120;
+
+  // Call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+
+  return timer;
+};
 ///////////////////////////////////////
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
 
 // FAke Always logged in
 // currentAccount = account1;
@@ -241,6 +265,10 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
+    // Timer
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -270,6 +298,10 @@ btnTransfer.addEventListener('click', function (e) {
 
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
@@ -288,7 +320,12 @@ btnLoan.addEventListener('click', function (e) {
 
       // Update UI
       updateUI(currentAccount);
+      
+      //Reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
     }, 2500);
+
   }
   inputLoanAmount.value = '';
 });
@@ -548,6 +585,7 @@ console.log(
   new Intl.NumberFormat(navigator.language, options).format(num)
 );*/
 
+// setTmeout
 const ingredients = ['olives', 'spinach'];
 const pizzaTimer = setTimeout(
   (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`),
@@ -563,3 +601,8 @@ if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
 //   const now = new Date();
 //   console.log(now);
 // }, 1000);
+
+// setInterval(function () {
+//   const now = new Date();
+//   console.log(`Clock \n${now.getHours()}hrs:${now.getMinutes()}mins:${now.getSeconds()}sec`);
+// },5000);
